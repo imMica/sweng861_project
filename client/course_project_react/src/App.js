31c05ee6ba141ab74b99cd26f7d7bbebbae45132
Marlene <m.marlene513@gmail.com>
2022-10-03 20:42:09 -0700

    added comment

--- client/course_project_react/src/App.js
+++ client/course_project_react/src/App.js
@@ -2,6 +2,7 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { BasicGrid } from './components/home.js'
 
+//set theme 
 const darkTheme = createTheme({
 	palette: {
 		mode: 'dark',
